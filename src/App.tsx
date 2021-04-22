d99990bb13e08f2b6e61d51df53ef0d2d5e0676a
ingridhoffman <ingridhoffman.pdx@gmail.com>
2021-04-22 11:17:59 -0700

    process api results

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,59 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import API from "./util/api";
 
+// custom types
+interface NewsItem {
+	id: string;
+	date: string;
+	title: string;
+	summary: string;
+	thumbnail: string;
+	link: string;
+}
+
+interface NewsFeed {
+	page: number;
+	totalPages: number;
+	results: NewsItem[];
+}
+
+// helper functions
+// to meet time constraints for this exercise I am not creating a type for the full api response and instead using any
+const processNewsResults = (res: any): NewsFeed => {
+	let itemArray: NewsItem[] = res.results.map((result: any) => {
+		let item: NewsItem = {
+			id: result.id,
+			date: new Date(result.webPublicationDate).toDateString(),
+			title: result.webTitle,
+			summary: result.fields.trailText,
+			thumbnail: result.fields.thumbnail,
+			link: result.webUrl,
+		};
+		return item;
+	});
+	return { page: res.currentPage, totalPages: res.pages, results: itemArray };
+};
+
 function App() {
 	const [loading, setLoading] = useState<boolean>(true);
-	const [newsContent, setNewsContent] = useState({});
+	const [newsResults, setNewsResults] = useState({});
 
 	useEffect(() => {
 		API.guardianContent()
 			.then((res) => {
-				console.log("results: ", res);
+				let newResults: NewsFeed = processNewsResults(res.data.response);
+				console.log(" new results: ", newResults);
+				setNewsResults(newResults);
+				setLoading(false);
 			})
 			.catch((err) => {
 				console.log("error: ", err.code);
 			});
 	}, []);
+
+	useEffect(() => {
+		console.log("news state: ", newsResults);
+	}, [newsResults]);
 
 	return <div className="App"></div>;
 }
--- src/util/api.js
+++ src/util/api.js
@@ -5,13 +5,15 @@ import axios from "axios";
 export default {
 	guardianContent: (searchWords, pageNumber) => {
 		const contentPath = "https://content.guardianapis.com/search?";
-		const specialRequests = "search-fields=thumbnail,trailText&";
+		const specialRequests = "show-fields=thumbnail,trailText&";
 		// env is used to make api key less visible but it is still not secure
 		// preferred approach would be to make the call from the server rather than the client
 		const permission = "api-key=" + process.env.REACT_APP_API_KEY;
 		let search = searchWords ? "q=" + searchWords + "&" : "";
 		let page = pageNumber ? "page=" + pageNumber + "&" : "";
 
-		return axios.get(contentPath + specialRequests + search + page + permission);
+		let query = contentPath + specialRequests + search + page + permission;
+		console.log("query: ", query);
+		return axios.get(query);
 	},
 };
